89160c74ceef854b010bd9d79a1c55551026d68d
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-06-25 15:52:19 +0800

    include/commands.h: implemented simple data structure for commands

--- include/commands.h
+++ include/commands.h
@@ -1,6 +1,27 @@
 # pragma once
 
-typedef struct s_commands
+# include "libft.h"
+
+enum e_str_type
 {
+	CMD,
+	OPTION,
+	ARG,
+	OPERATOR
+};
+
+typedef struct s_cmd
+{
+	char	*raw;
 	char	**argv;
-}	t_commands;
+	int		*type;
+}	t_cmd;
+
+typedef struct s_cmd_table
+{
+	t_list	*cmd;
+	int		cmd_count;
+}	t_cmd_table;
+
+t_cmd	*new_cmd(const char *line);
+void	free_cmd(void *cmd);
