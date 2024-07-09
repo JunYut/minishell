7f03fa768b6f3688b6af5376a0137f6d151fcd4b
Tan Jun Yu <tjun-yu@u82z05s04.42kl.edu.my>
2024-07-09 14:18:29 +0800

    chore: moved test files into sandbox/

--- junyu/pipe_module/pipe.c
+++ junyu/pipe_module/pipe.c
@@ -5,20 +5,15 @@ void	pipex(char *cmds[], char ***args, int fd[][2], int pipe_count)
 	pid_t	pid;
 	int		i;
 
-	(void)cmds;
-	(void)args;
-	(void)fd;
-	(void)pipe_count;
 	i = -1;
 	while (++i < pipe_count + 1)
 	{
 		pid = fork();
 		if (pid == 0)
 		{
-			// printf("child [%d]: %d\n", i, getpid());
-			// close_fds(fd, i, pipe_count);
 			redirect(fd, i, pipe_count);
-			// execve(cmds[i], args[i], NULL);
+			close_fds(fd, i, pipe_count);
+			execve(cmds[i], args[i], NULL);
 			exit(0);
 		}
 	}
@@ -28,11 +23,11 @@ void	pipex(char *cmds[], char ***args, int fd[][2], int pipe_count)
 // not redirecting properly
 void	redirect(int fd[][2], int i, int pipe_count)
 {
-	int	stdout_fd;
-	int	stdin_fd;
+	// int	stdout_fd;
+	// int	stdin_fd;
 
-	stdout_fd = dup(STDOUT_FILENO);
-	stdin_fd = dup(STDIN_FILENO);
+	// stdout_fd = dup(STDOUT_FILENO);
+	// stdin_fd = dup(STDIN_FILENO);
 	if (i == pipe_count)
 		return ;
 	// printf("i: %d\n", i);
@@ -47,32 +42,23 @@ void	redirect(int fd[][2], int i, int pipe_count)
 		dup2(fd[i - 1][0], STDIN_FILENO);
 		dup2(fd[i][1], STDOUT_FILENO);
 	}
-	dup2(stdout_fd, STDOUT_FILENO);
-	dup2(stdin_fd, STDIN_FILENO);
-	close(stdout_fd);
-	close(stdin_fd);
+	// dup2(stdout_fd, STDOUT_FILENO);
+	// dup2(stdin_fd, STDIN_FILENO);
+	// close(stdout_fd);
+	// close(stdin_fd);
 	// printf("redirected\n");
 }
 
 // i is the index of the current pipe
 void	close_fds(int fd[][2], int i, int pipe_count)
 {
+	(void)i;
 	int	j;
 
-	// printf("i: %d\n", i);
-	// printf("closing fds\n");
-	if (i == 0)
-		close(fd[0][0]);
-	// printf("closed fd[0][0]\n");
 	j = -1;
 	while (++j < pipe_count)
 	{
-		if (i > 0 && j > 0)
-			close(fd[j - 1][1]);
 		close(fd[j][0]);
+		close(fd[j][1]);
 	}
-	// printf("closed most fds\n");
-	if (i == pipe_count - 1)
-		close(fd[pipe_count - 1][1]);
-	// printf("closed all fds\n");
 }
