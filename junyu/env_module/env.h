dc727aa52feae3031c68b4c5edd951525a5bc13d
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-07-23 16:46:40 +0800

    feat(env_module): implemented pwd() and echo()

--- junyu/env_module/env.h
+++ junyu/env_module/env.h
@@ -32,6 +32,8 @@ void	env(t_env *e, char lst);
 void	export(char *str, t_env *e);
 void	unset(char *key, t_env *e);
 void	cd(char *path, t_env *e);
+void	pwd(t_env *e);
+void	echo(char **av);
 
 void	add_var(t_env *e, char *key, char *val);
 void	set_val(t_env *e, char *key, char *val);
--- junyu/env_module/main.c
+++ junyu/env_module/main.c
@@ -13,8 +13,6 @@ int main(int ac, char **av, char **envp)
 
 	e = dup_env(envp);
 
-	env(e, VAR);
-	// printf("\n");
 	# if TEST == 1
 	printf("cwd: %s\n", gb_add(getcwd(NULL, 0)));
 	printf("PWD: %s\n", fetch_val("PWD", e));
@@ -105,6 +103,15 @@ int main(int ac, char **av, char **envp)
 	printf("OLDPWD: %s\n", fetch_val("OLDPWD", e));
 	printf("\n");
 	# endif
+
+	# if TEST == 2
+	pwd(e);
+	cd("../", e);
+	pwd(e);
+	#endif
+
+	char *strs[] = {"Hello", "World", NULL};
+	echo(strs);
 
 	gb_clear();
 	return 0;
