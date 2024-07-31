2277a87beb0fb9c16623c0b9c7c3798075db5080
Tan Jun Yu <tjun-yu@u82z05s04.42kl.edu.my>
2024-07-31 10:57:58 +0800

    chore(signal_module): updated notes in signal.h regarding interactive actions

--- signal_module/signal.h
+++ signal_module/signal.h
@@ -8,12 +8,13 @@ Non-interactive mode:
 - ctrl-D does nothing (no need to implement this).
 - ctrl-\ displays '^\Quit: 3' then a new prompt on a new line.
 
-Interactive mode(involves real-time user input):
+Interactive mode(involves real-time user input by shell builtins):
 - ctrl-C displays a new prompt on a new line.
 - ctrl-D exits interactive mode. (no need to implement this)
 - ctrl-\ does nothing.
 
-Interactive actions: read, <<heredoc
+Interactive actions in bash:  prompt, read, <<heredoc, unclosed quotes
+Interactive actions in minishell: prompt, <<heredoc, unclosed quotes
 */
 
 # include <signal.h>
