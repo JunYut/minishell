a35b2877ab67c2d506cafe2578f31c7478592a84
Tan Jun Yu <tjun-yu@u82z05s04.42kl.edu.my>
2024-08-05 12:59:49 +0800

    fix(exec/redir): now no more leaks

--- exec/main.c
+++ exec/main.c
@@ -1,7 +1,20 @@
+# include "redir.h"
 # include "libft.h"
+# include "gbc.h"
+
+# define TEST 1
 
 int main(void)
 {
-	ft_printf("Hello, World!\n");
-	return (0);
+	# if TEST == 1
+	char	*cmd = NULL;
+	char	*args[] = {NULL};
+	# endif
+	# if TEST == 0
+	char	*cmd = "/usr/bin/cat";
+	char	*args[] = {"cat", "-e", NULL};
+	# endif
+
+	heredoc("eof", cmd, args);
+	// printf("[%s]\n", readline("> "));
 }
