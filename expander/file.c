b18ec95f0e894340f44ce27ffe2221c32a6c124a
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-09-06 15:54:38 +0800

    refactor(expander): split `temp.c` into `file.c` and `path.c`

--- expander/file.c
+++ expander/file.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   temp.c                                             :+:      :+:    :+:   */
+/*   file.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 15:18:06 by we                #+#    #+#             */
-/*   Updated: 2024/09/06 15:18:07 by we               ###   ########.fr       */
+/*   Updated: 2024/09/06 15:53:03 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,63 +61,4 @@ char	*read_pipe(int read_fd)
 	}
 	free(tmp);
 	return (content);
-}
-
-void	append_str(char **str1, char *str2)
-{
-	int		i;
-	int		j;
-	char	*new;
-
-	i = -1;
-	j = -1;
-	new = gbc_malloc(ft_strlen(*str1) + ft_strlen(str2) + 1);
-	while ((*str1)[++i])
-		new[i] = (*str1)[i];
-	while (str2[++j])
-		new[i + j] = str2[j];
-	new[i + j] = '\0';
-	*str1 = new;
-}
-
-char	*parse_path(char *envp[], char *cmd)
-{
-	char	**path_list;
-	char	*path;
-	int		i;
-
-	i = -1;
-	while (envp[++i] && ft_strncmp(envp[i], "PATH=", 5))
-		;
-	path_list = ft_split(envp[i], ':');
-	prepend_cmd(path_list, cmd);
-	trim_path(path_list[0], "PATH=");
-	path = NULL;
-	i = -1;
-	while (path_list[++i] && !path)
-		if (access(path_list[i], X_OK) == 0)
-			path = ft_strdup(path_list[i]);
-	ft_free_s_arr(path_list);
-	return (path);
-}
-
-void	trim_path(char *cmd, char *path)
-{
-	size_t	path_len;
-
-	path_len = ft_strlen(path);
-	ft_memmove(cmd, cmd + path_len, ft_strlen(cmd) - path_len);
-	cmd[ft_strlen(cmd) - path_len] = '\0';
-}
-
-void	prepend_cmd(char **path_list, char *cmd)
-{
-	int	i;
-
-	i = -1;
-	while (path_list[++i])
-	{
-		path_list[i] = gnl_strjoin(path_list[i], "/");
-		path_list[i] = gnl_strjoin(path_list[i], cmd);
-	}
 }
