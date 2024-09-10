8c39822d26c68ba5441f95563630ed81cf22a61c
Tan Jun Yu <tjun-yu@u91z06s03.42kl.edu.my>
2024-09-10 12:53:59 +0800

    fix(builtins): now `exit_shell()` will not cause segfault when there are no arguments

--- builtins/exit.c
+++ builtins/exit.c
@@ -3,34 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 14:06:53 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2024/09/05 11:55:35 by we               ###   ########.fr       */
+/*   Updated: 2024/09/10 12:48:27 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exit_shell(char **status, t_env *e)
+void	exit_shell(char **argv, int status, t_env *e)
 {
 	int	exit_status;
 
 	printf("exit\n");
-	if (status && status[0])
+	if (argv && argv[0])
 	{
-		if (!is_numeric(status[0]))
+		if (!is_numeric(argv[0]))
 		{
-			print_builtin_err("exit", status[0], "numeric argument required");
+			print_builtin_err("exit", argv[0], "numeric argument required");
 			exit_status = 2;
 		}
-		else if (count_args(status, "exit", e) > 1)
+		else if (count_args(argv, "exit", e) > 1)
 			exit_status = 1;
 		else
-			exit_status = ft_atoi(status[0]);
+			exit_status = ft_atoi(argv[0]);
 	}
-	if (!status || !status[0] || status[0][0] == '\0')
-		exit_status = ft_atoi(fetch_val("?", e));
+	if (!argv || !argv[0] || argv[0][0] == '\0')
+		exit_status = status;
 	gbc_clear();
 	exit(exit_status);
 }
