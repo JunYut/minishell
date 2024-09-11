a88e9e7df21459605f007af3b67bf6dc2f471e54
Kau Khai Kit <kkhai-ki@student.42kl.edu.my>
2024-09-11 19:46:50 +0800

    fix(expander): expansion handlers now use gbc for easier memory management

--- expander/handle.c
+++ expander/handle.c
@@ -6,7 +6,7 @@
 /*   By: kkhai-ki <kkhai-ki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 11:21:12 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/09/11 19:16:22 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2024/09/11 19:45:33 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*handle_reg_str(char *str, int *i)
 	while (str[*i] != '\0' && str[*i] != '\''
 		&& str[*i] != '"' && str[*i] != '$')
 		(*i)++;
-	return (ft_substr(str, start, *i - start));
+	return (gbc_add(ft_substr(str, start, *i - start)));
 }
 
 char	*handle_squote(char *str, int *i)
@@ -32,7 +32,7 @@ char	*handle_squote(char *str, int *i)
 	while (str[*i] != '\'')
 		(*i)++;
 	(*i)++;
-	return (ft_substr(str, start, *i - start));
+	return (gbc_add(ft_substr(str, start, *i - start)));
 }
 
 char	*handle_dquote_str(char *str, int *i)
@@ -42,7 +42,7 @@ char	*handle_dquote_str(char *str, int *i)
 	start = *i;
 	while (str[*i] != '"' && str[*i] != '$')
 		(*i)++;
-	return (ft_substr(str, start, *i - start));
+	return (gbc_add(ft_substr(str, start, *i - start)));
 }
 
 char	*handle_dquote(char *str, int *i, t_minishell *vars)
@@ -70,21 +70,21 @@ char	*handle_dollar(char *str, int *i, t_minishell *vars)
 
 	(*i)++;
 	if (str[*i] == '\0' || str[*i] == '"' || str[*i] == ' ')
-		return (ft_strdup("$"));
+		return (gbc_add(ft_strdup("$")));
 	start = *i;
 	if (str[*i] == '?' || is_valid_var_char(str[*i]) == false)
 	{
 		(*i)++;
 		if (str[*i - 1] == '?')
-			return (ft_strdup(ft_itoa(vars->exit_status)));
+			return (gbc_add(ft_strdup(ft_itoa(vars->exit_status))));
 		if (is_valid_var_char(str[*i - 1]) == false)
-			return (ft_substr(str, start - 1, *i - start + 1));
+			return (gbc_add(ft_substr(str, start - 1, *i - start + 1)));
 	}
 	while (is_valid_var_char(str[*i]) == true)
 		(*i)++;
 	val = ft_substr(str, start, *i - start);
 	env_val = fetch_val(val, vars->env);
 	if (env_val == NULL)
-		return (free(val), ft_strdup(""));
+		return (free(val), gbc_add(ft_strdup("")));
 	return (free(val), env_val);
 }
