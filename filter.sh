ea031fc89a43ea0e464f9d453c908c787309d271
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-09-12 12:54:40 +0800

    issue(heredoc): leaks in `expand_heredoc()`->`handle_non_var()`->`ft_substr()`

--- filter.sh
+++ filter.sh
@@ -1,3 +1,4 @@
-grep -v readline valgrind-output.txt| grep -v rl | grep -v '/usr' | grep -v strdup.c |\
- grep -v rl | grep -v xmalloc | grep -v display.c | grep -v bind.c |\
- grep -v tsearch.c | grep -v history.c | grep -v terminal.c | grep -v main.c > valgrind-output.txt
+grep -v readline valgrind-output.txt| grep -v rl | grep -v '/usr' |\
+ grep -v strdup.c | grep -v rl | grep -v xmalloc | grep -v display.c |\
+ grep -v bind.c | grep -v tsearch.c | grep -v history.c | grep -v terminal.c |\
+ grep -v main.c | egrep -v ' $' | egrep -v '[0-9]$' > valgrind-output.txt
